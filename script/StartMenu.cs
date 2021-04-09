3b880701e66a02c00a61ca8e719815ae86a134d1
Rifqi IR <74804470+RestInReal@users.noreply.github.com>
2021-04-09 16:52:11 +0700

    Add files via upload

---
+++ script/StartMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void sceneSelect()
+    {
+        SceneManager.LoadScene("main");
+    }
+    // Update is called once per frame
+    public void quitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
